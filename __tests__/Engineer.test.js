ddd5dd6e2d3b8073a75172ae16e73e6222afbf68
EddieH <ehajek@outlook.com>
2021-07-31 14:31:05 -0400

    Second Test - Engineer

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('@jest/globals');
+const Engineer = require('../lib/Engineer.js');
+
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Frank', 36, 'frank@gmail.com', 'frankHub');
+  
+    expect(engineer.name).toBe('Frank');
+    expect(engineer.id).toEqual(36);
+    expect(engineer.email).toEqual('frank@gmail.com');
+    expect(engineer.gitHubUser).toEqual('frankHub')
+  //name
+  //id
+  //email
+  //
+  });
